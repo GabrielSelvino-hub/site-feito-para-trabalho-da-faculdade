e0d49ff34ed5f7e4af980adc90f1d3bf600e8ab3
Gabriel Selvino <selvino.gabriel@hotmail.com>
2020-11-06 23:46:56 -0300

    nome da pasta

--- CONTATO/javascript.js
+++
@@ -1,7 +0,0 @@
-		
-		//Um pequena função em JavaScript que mostra uma mensagem quando clicar no botão enviar mensagem na página//
-	
-		function obrigado() {
-			alert("Obrigado por nos enviar uma mensagem! Responderemos em breve");
-		}
-		
---
+++ site-contato/javascript.js
@@ -0,0 +1,7 @@
+		
+		//Um pequena função em JavaScript que mostra uma mensagem quando clicar no botão enviar mensagem na página//
+	
+		function obrigado() {
+			alert("Obrigado por nos enviar uma mensagem! Responderemos em breve");
+		}
+		
